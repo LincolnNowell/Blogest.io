b1b83b080cb1b12330fb5369ddc6b85399a6f1b8
Lincoln Nowell <lincolnnowell@gmail.com>
2020-01-30 05:47:31 -0600

    !

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.urlencoded());
+
+
+app.use("/imgs",express.static(__dirname + '/imgs'));
+//app.use("/scripts",express.static(__dirname + '/scripts'));
+app.use('/pages',express.static(__dirname + '/pages'));
+
+app.get('/',(req,res) =>{
+    res.sendFile(path.join(__dirname,'index.html'));
+});
+
+app.get('*', function(req, res) {
+    res.redirect('/');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,() =>{
+    console.log(`Server started on port ${PORT}`);
+});
